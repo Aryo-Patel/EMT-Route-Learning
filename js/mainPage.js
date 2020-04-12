b137bdb09e782606196e9ed065b87c63f483e2b4
AryoP <aryopatel@gmail.com>
2020-04-12 13:59:30 -0500

    changed the radius input to a user inputting the locations they want to reach

--- js/mainPage.js
+++ js/mainPage.js
@@ -1,20 +1,30 @@
 let mapCenterLocation = document.getElementById('center-location');
-let mapRadius = document.getElementById('mile-radius');
+let mapInputs = document.getElementById('map-input');
+let addPlaces = document.getElementById('add-places');
+let add = document.getElementById('add');
+
+add.addEventListener('click', e => mapInfo(e));
+
+function deletePlace(e) {
+    e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+}
 
 
-function mapInfo (){
-    if(mapCenterLocation.value && mapRadius.value){
-        let location = mapCenterLocation.value;
-        let radius = mapRadius.value;
-        console.log(radius)
-        mapCenterLocation.value = '';
-        mapRadius.value = '';
-        return {
-            location: location,
-            radius: radius
-        }
-    }
-    else{
-        console.log('missing one or two inputs');
+//Adds to the list of places the user needs to know how to get to.
+function mapInfo (e){
+    e.preventDefault();
+    if(mapCenterLocation.value && mapInputs.value){
+        let places = mapInputs.value.split(', ');
+        places.forEach(place =>{
+            let placeContainer = document.createElement('li');
+            placeContainer.addEventListener('click', e => deletePlace(e));
+            let placeText = document.createElement('span');
+            placeText.textContent = place;
+            placeContainer.appendChild(placeText);
+            addPlaces.appendChild(placeContainer);
+        })
+        addPlaces.scrollTop = addPlaces.scrollHeight;
+
+        mapInputs.value = '';
     }
 }
